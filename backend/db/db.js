e22089586c449c65bf212c0dfb495399d333d410
dhanush106 <sarpoordhanush@gmail.com>
2026-07-04 17:00:02 +0530

    (feat):-DB connected successfully

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,1 +1,12 @@
 import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try{
+        const connectionURI = await mongoose.connect(process.env.MONGO_URI);
+        console.log(connectionURI.connection.host, "MongoDB connected successfully");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './db/db.js';
 import express from 'express';
 
 const app = express();
-
-app.listen(process.env.PORT || 3001,()=>{
-    console.log(`Server is running on port ${process.env.PORT || 3001}`);
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 3001,()=>{
+        console.log(`Server is running on port ${process.env.PORT || 3001}`);
+    })    
 })
