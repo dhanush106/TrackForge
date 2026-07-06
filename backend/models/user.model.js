b04acd55046b358bc96a0d416fca7af1eb15cf70
dhanush106 <sarpoordhanush@gmail.com>
2026-07-06 15:17:46 +0530

    (feat):-Implemented password  hashing

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from 'mongoose';
-
+import bcrypt from 'bcrypt';
 const userSchema = new Schema({
     name : {
         type: String,
@@ -28,5 +28,15 @@ const userSchema = new Schema({
         type: String
     }
 },{timestamps: true});
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+})
+
+userSchema.methods.isPassWordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
 
 const User = Schema.model('User', userSchema);
